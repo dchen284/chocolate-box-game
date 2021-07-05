1f79621b2508c6b99a853d89f99e77a1dd87d733
Danny <dchen284@gmail.com>
2021-07-04 19:48:58 -0500

    rename tokens to tiles

--- react-app/src/components/game/GameDisplay.js
+++ react-app/src/components/game/GameDisplay.js
@@ -2,18 +2,6 @@
 import React from 'react';
 // Internal imports
 import GameSpace from './GameSpace';
-// import tokenDark1 from '../../images/tokens/token_dark-1.png';
-// import tokenDark2 from '../../images/tokens/token_dark-2.png';
-// import tokenDark3 from '../../images/tokens/token_dark-3.png';
-// import tokenDark4 from '../../images/tokens/token_dark-4.png';
-// import tokenMilk1 from '../../images/tokens/token_milk-1.png';
-// import tokenMilk2 from '../../images/tokens/token_milk-2.png';
-// import tokenMilk3 from '../../images/tokens/token_milk-3.png';
-// import tokenMilk4 from '../../images/tokens/token_milk-4.png';
-// import tokenWhite1 from '../../images/tokens/token_white-1.png';
-// import tokenWhite2 from '../../images/tokens/token_white-2.png';
-// import tokenWhite3 from '../../images/tokens/token_white-3.png';
-// import tokenWhite4 from '../../images/tokens/token_white-4.png';
 import './GameDisplay.css';
 
 
