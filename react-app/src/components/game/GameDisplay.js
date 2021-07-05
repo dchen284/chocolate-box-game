40b612f76317a01184b9a3a7d6b110e87d2daf31
Danny <dchen284@gmail.com>
2021-07-05 14:36:15 -0500

    logic for legal placements

--- react-app/src/components/game/GameDisplay.js
+++ react-app/src/components/game/GameDisplay.js
@@ -9,9 +9,6 @@ import './GameDisplay.css';
 
 const GameDisplay = () => {
 
-    const [tiles, setTiles] = useState(["D1", "D1", "D1"]);
-
-
     const numberOfRows = 5;
     const numberOfColumns = 5;
     const initialBoardState = new Array(numberOfRows)
@@ -23,30 +20,174 @@ const GameDisplay = () => {
         }
     }
 
-    const boardStateToString = (boardState) => {
-        let strOutput = "";
-        for (let i = 0; i < numberOfRows; i++) {
-            for (let j = 0; j < numberOfColumns; j++) {
-                strOutput += `${boardState[i][j]},`;
-            }
-        }
-        strOutput = strOutput.slice(0, strOutput.length - 1);
-        console.log('strOutput', strOutput);
-        return strOutput;
-    }
-
     initialBoardState[1][2] = "M3";
-    boardStateToString(initialBoardState);
+    initialBoardState[4][3] = "D2";
 
     const [boardState, setBoardState] = useState(initialBoardState);
+    const [currentTile, setCurrentTile] = useState("00");
+    const [tilesRemaining, setTilesRemaining] = useState(["D1", "D1", "D1"]);
+
+
+    // const boardStateToString = (boardState) => {
+    //     let strOutput = "";
+    //     for (let i = 0; i < numberOfRows; i++) {
+    //         for (let j = 0; j < numberOfColumns; j++) {
+    //             strOutput += `${boardState[i][j]},`;
+    //         }
+    //     }
+    //     strOutput = strOutput.slice(0, strOutput.length - 1);
+    //     console.log('strOutput', strOutput);
+    //     return strOutput;
+    // }
+
+
+    // boardStateToString(initialBoardState);
+
+    const createDeepCopyOfBoardState = () => {
+        const copy = new Array(numberOfRows)
+
+        for (let i = 0; i < numberOfRows; i++) {
+            copy[i] = new Array(numberOfColumns);
+            for (let j = 0; j < numberOfColumns; j++) {
+                copy[i][j] = "00"
+            }
+        }
+
+        for (let i = 0; i < numberOfRows; i++) {
+            for (let j = 0; j < numberOfColumns; j++) {
+                copy[i][j] = boardState[i][j];
+            }
+        }
+        return copy;
+    }
+
+    // useEffect(()=>{
+    //     const createDeepCopyOfBoardState = () => {
+    //         const copy = new Array(numberOfRows)
+
+    //         for (let i = 0; i < numberOfRows; i++) {
+    //             copy[i] = new Array(numberOfColumns);
+    //             for (let j = 0; j < numberOfColumns; j++) {
+    //                 copy[i][j] = "00"
+    //             }
+    //         }
+
+    //         for (let i = 0; i < numberOfRows; i++) {
+    //             for (let j = 0; j < numberOfColumns; j++) {
+    //                 copy[i][j] = boardState[i][j];
+    //             }
+    //         }
+    //         return copy;
+    //     }
+
+    //     const copy = createDeepCopyOfBoardState();
+    //     copy[1][2] = "M3";
+    //     copy[4][3] = "D2";
+    //     setBoardState(copy);
+    // },[])
 
     useEffect(()=>{
         const strTiles = 'D1,M3,W2,D4,M4,W4';
-        setTiles(strTiles.split(","));
+        setTilesRemaining(strTiles.split(","));
+        // console.log('useEffect fired');
     }, [])
 
-    const placeTile = () => {
 
+    const getNeighbors = (row, col) => {
+        // console.log(coord);
+        const neighbors = [];
+        // const [row, col] = [+coord[0], +coord[1]]; //row and col are numbers
+        if (0 <= row - 1) {neighbors.push([row-1, col])}
+        if (0 <= col - 1) {neighbors.push([row, col-1])}
+        if (row+1 < numberOfRows) {neighbors.push([row+1, col])}
+        if (col+1 < numberOfColumns) {neighbors.push([row, col+1])}
+
+        return neighbors;
+        // const currSpace = boardState[row][col];
+        // const legalNeighbors = neighborSpaces.filter(neighbor => {
+        //     if (neighbor !== "00") {
+        //         if (currSpace[0] === neighbor[0] || currSpace[1] === neighbor[1]) {
+        //             return true;
+        //         }
+        //     }
+        //     return false;
+        // })
+
+
+    }
+
+    const checkLegalSpace = (row, col) => {
+        let boolIsLegal = true;
+        const neighbors = getNeighbors(row, col);
+        // const [row, col] = [+coord[0], +coord[1]]; //row and col are numbers
+
+        const currSpace = boardState[row][col];
+
+        //if the space is already occupied, the move is not legal
+        if (currSpace !== "00") {
+            console.log('spaces occupied')
+            boolIsLegal = false
+        };
+
+        //if the space's neighbors are all empty (00), the move is not legal
+        let allNeighborsEmpty = true;
+        neighbors.forEach( neighbor => {
+            const [neighborRow, neighborCol] = neighbor;
+            const neighborValue = boardState[neighborRow][neighborCol];
+            if (neighborValue !== "00") {allNeighborsEmpty = false}
+        });
+        if (allNeighborsEmpty) {
+            console.log('all neighbors empty')
+            boolIsLegal = false
+        };
+
+        //for each space's occupied neighbors, if either the letter or number do not match, the
+        //move is not legal
+
+        neighbors.forEach( neighbor => {
+            const [neighborRow, neighborCol] = neighbor;
+            const neighborValue = boardState[neighborRow][neighborCol];
+            if (neighborValue !== "00" && currentTile[0] !== neighborValue[0] && currentTile[1] !== neighborValue[1]) {
+                console.log('did not match')
+                boolIsLegal = false;
+            }
+        });
+
+        console.log(boolIsLegal);
+        return boolIsLegal;
+    }
+
+    const placeTile = (e) => {
+        // console.log('placeTile fired'
+        console.log('currentTile', currentTile);
+        const targetId = e.target.id;
+        const targetCoord = targetId.slice(1, 3);
+        const [targetRow, targetCol] = [+targetCoord[0], +targetCoord[1]]; //targetRow and targetCol are strings
+        // checkLegalSpace(targetRow, targetCol);
+        if (checkLegalSpace(targetRow, targetCol)) {
+            console.log('placing tile');
+            const newBoardState = createDeepCopyOfBoardState();
+            newBoardState[targetRow][targetCol] = currentTile;
+            setBoardState(newBoardState);
+            const newTilesRemaining = [...tilesRemaining];
+            newTilesRemaining.splice(tilesRemaining.indexOf(currentTile), 1);
+            setTilesRemaining(newTilesRemaining);
+            console.log('reset current tile');
+            setCurrentTile("00");
+        }
+    }
+
+    // console.log('tiles in base file', tilesRemaining);
+
+    const selectTile = (e) => {
+        // console.log('tiles in selectTile', tilesRemaining);
+        const targetId = e.target.id;
+        const indexOfDash = targetId.indexOf("-")
+        if (indexOfDash !== -1) {
+            const strTile = targetId.slice(indexOfDash+1, targetId.length);
+            setCurrentTile(strTile);
+            console.log('updated new tile');
+        }
     }
 
     // JSX
@@ -55,17 +196,15 @@ const GameDisplay = () => {
         <>
             <h1>Game</h1>
             <div
-            onClick={() => {
-                setTiles(tiles.slice(1));
-                console.log(tiles);}}
+            onClick={placeTile}
             >
                 <BoardDisplay boardState={boardState}/>
             </div>
 
             <div
-            onClick={(e) => {if (e.target.id) console.log(e.target.id)}}
+            onClick={selectTile}
             >
-                <TilesDisplay arrTiles={tiles}/>
+                <TilesDisplay arrTiles={tilesRemaining}/>
             </div>
         </>
     )
