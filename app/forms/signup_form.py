aa03fba0635907225df513e16356a8121f0464d9
Danny <dchen284@gmail.com>
2021-08-05 21:51:18 -0500

    add new validations to signup, change css to allow spacing for error messages

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Regexp, ValidationError
 from app.models import User
 
+regex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$"
 
 def user_exists(form, field):
     # Checking if user exists
@@ -22,7 +23,21 @@ def username_exists(form, field):
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired(), EqualTo('repeat_password', message='Passwords must match')])
+        'username', validators=[
+                                DataRequired(),
+                                Length(min=3, max=40,
+                                       message="Username must be between 3 to 40 characters long."),
+                                username_exists,
+                                ])
+    email = StringField('email', validators=[DataRequired(), Email(), user_exists])
+    password = StringField('password',
+                            validators=[DataRequired(),
+                                        Regexp(regex,
+                                               message=("Minimum eight characters, "
+                                                        "at least one upper case English letter, "
+                                                        "one lower case English letter, one number "
+                                                        "and one special character.")
+                                              ),
+                                        EqualTo('repeat_password', message='Passwords must match')
+                                        ])
     repeat_password = StringField('password', validators=[DataRequired()])
